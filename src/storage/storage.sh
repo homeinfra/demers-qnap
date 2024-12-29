bcda1b5b8774ba08aa2f78a5059375b75ee7f649
Jérémie Faucher-Goulet <jeremiefauchergoulet@hotmail.com>
2024-12-29 03:40:31 -0500

    WIP on storage

--- src/storage/storage.sh
+++ src/storage/storage.sh
@@ -4,18 +4,142 @@
 # This script configures the persistent storage for the XCP-ng host
 
 storage_setup() {
+  STOR_FILE="${CONFIG_DIR}/storage.env"
+
+  local res=0
+  while [[ ${res} -eq 0 ]]; do
+    # Load the new configuration
+    config_load "${STOR_FILE}"
+
+    if [[ -z ${STOR_STATE} ]]; then
+      storage_design
+      res=$?
+    else
+      case ${STOR_STATE} in
+      designed)
+        storage_create
+        res=$?
+        ;;
+      created)
+        storage_mount
+        res=$?
+        ;;
+      *)
+        logError "Unknown state: ${STOR_STATE}"
+        return 1
+        ;;
+      esac
+    fi
+  done
+
+  return ${res}
+}
+
+storage_create() {
+  if [[ -z ${VM_STOR_DRIVE1} ]] || [[ -z ${VM_STOR_DRIVE2} ]] || [[ -z ${VM_STOR_DRIVE1_START} ]] || [[ -z ${VM_STOR_DRIVE2_START} ]] || [[ -z ${VM_STOR_SIZE} ]]; then
+    logError "Missing configuration for VM storage"
+    return 1
+  fi
+  if [[ ${VM_STOR_DRIVE1} == ${VM_STOR_DRIVE2} ]]; then
+    logError "VM storage drives are the same"
+    return 1
+  fi
+  if [[ ${VM_STOR_DRIVE1_START} -ne 0 ]] || [[ ${VM_STOR_DRIVE2_START} -le 0 ]] || [[ ${VM_STOR_SIZE} -le 0 ]]; then
+    logError "Invalid configuration for VM storage"
+    return 1
+  fi
+  if [[ -z ${ISO_STOR_DRIVE} ]] || [[ -z ${ISO_STOR_START} ]] || [[ -z ${ISO_STOR_SIZE} ]]; then
+    logError "Missing configuration for ISO storage"
+    return 1
+  fi
+
+    local vm_loop_device
+    local iso_loop_device
+    local res=0
+
+  # If we reach here, we've confirmed the need to create a loop device for drive 2
+  if ! disk_create_loop vm_loop_device "${VM_STOR_DRIVE2}" "${VM_STOR_DRIVE2_START}" "${VM_STOR_SIZE}"; then
+    logError "Failed to create loop device for VM storage"
+    return 1
+  elif ! config_save "${STOR_FILE}" VM_STOR_LOOP2 "${vm_loop_device}"; then
+    logError "Failed to save VM_STOR_LOOP2"
+    return 1
+  else
+    logInfo "Loop device created for VM storage: ${vm_loop_device}"
+  fi
+
+  # Create the RAID1 array
+  if ! disk_create_raid1 "${VM_STOR_DRIVE}" "${VM_STOR_DRIVE1}" "${vm_loop_device}"; then
+    logError "Failed to create RAID1 array for VM storage"
+    return 1
+  else
+    logInfo "RAID1 array created for VM storage"
+  fi
+
+  # Create the ISO storage
+  if ! disk_create_loop iso_loop_device "${ISO_STOR_DRIVE}" "${ISO_STOR_START}" "${ISO_STOR_SIZE}"; then
+    logError "Failed to create loop device for ISO storage"
+    return 1
+  elif ! config_save "${STOR_FILE}" ISO_STOR_LOOP "${iso_loop_device}"; then
+    logError "Failed to save ISO_STOR_LOOP"
+    return 1
+  else
+    logInfo "Loop device created for ISO storage: ${iso_loop_device}"
+  fi
+
+  # Format the ISO storage
+  if ! disk_format "${iso_loop_device}" "ext4"; then
+    logError "Failed to format ISO storage"
+    return 1
+  else
+    logInfo "ISO storage formatted"
+  fi
+
+  # # Save the configuration as created
+  # if ! config_save "${STOR_FILE}" STOR_STATE created; then
+  #   logError "Failed to save STOR_STATE"
+  #   return 1
+  # else
+  #   logInfo "Storage created"
+  # fi
+
+  # If we reached here, everything was created successfully. Now unload it all
+  if ! disk_remove_raid "${VM_STOR_DRIVE}"; then
+    logError "Failed to remove RAID array"
+    res=1
+  else
+    logInfo "RAID array removed"
+  fi
+  if ! disk_remove_loop "${vm_loop_device}"; then
+    logError "Failed to remove loop device for VM storage"
+    res=1
+  else
+    logInfo "Loop device removed for VM storage"
+  fi
+  if ! disk_remove_loop "${iso_loop_device}"; then
+    logError "Failed to remove loop device for ISO storage"
+    res=1
+  else
+    logInfo "Loop device removed for ISO storage"
+    res=2 # TODO: Break the loop
+  fi
+
+  return ${res}
+}
+
+storage_design() {
   local boot_drive
   local boot_partition
   local local_drives
 
-  if ! disk_get_drives local_drives; then
+  if ! disk_list_drives local_drives; then
     logError "Failed to retrieve local drives"
     return 1
   else
     logInfo "There are ${#local_drives[@]} local drives: ${local_drives[*]}"
   fi
 
-  if ! disk_boot_partition boot_drive boot_partition; then
+  if ! disk_root_partition boot_drive boot_partition; then
     logError "Failed to retrieve boot drive and partition"
     return 1
   else
@@ -25,18 +149,21 @@ storage_setup() {
   # Build an associative array of drives and their sizes
   declare -A drive_sectors
   declare -A drive_sector_sizes
+  declare -A drive_physical_sectors
   declare -A drive_start_sectors
   declare -A drive_end_sectors
   local drive
   local nb_sectors
   local size_sectors
+  local phys_sectors
   local start_sector
   local end_sector
   for drive in "${local_drives[@]}"; do
     # Get the size of the drive
-    if disk_drive_size nb_sectors size_sectors "${drive}"; then
+    if disk_drive_size nb_sectors size_sectors phys_sectors "${drive}"; then
       drive_sectors["$drive"]="${nb_sectors}"
       drive_sector_sizes["$drive"]="${size_sectors}"
+      drive_physical_sectors["$drive"]="${phys_sectors}"
     else
       logError "Failed to get size of: ${drive}"
       return 1
@@ -55,32 +182,130 @@ storage_setup() {
 Summary for all drives:
 $(for drive in "${local_drives[@]}"; do
   __avail=$(( (${drive_end_sectors[$drive]} - ${drive_start_sectors[$drive]}) ))
-  __availGiB=$(( ${__avail} * ${drive_sector_sizes[$drive]} / 1024 / 1024 / 1024 ))
+  __availGiB=$(( ${__avail} * 512 / 1024 / 1024 / 1024 ))
   echo ""
   echo "Drive: ${drive}"
   echo "  Size: ${drive_sectors[$drive]} sectors"
   echo "  Sector size: ${drive_sector_sizes[$drive]}"
+  echo "  Physical sector size: ${drive_physical_sectors[$drive]}"
   echo "  Available space: ${__avail} sectors (${__availGiB} GiB)"
   echo "    Start sector     : ${drive_start_sectors[$drive]}"
   echo "    End sector       : ${drive_end_sectors[$drive]}"
 done)
 EOF
 
-#     logInfo <<EOF
-# We could create a partition on ${drive}:
-#   Start sector     : ${start_sector}
-#   End sector       : ${end_sector}
-#   Number of sectors: $(( ${end_sector} - ${start_sector} ))
-#   Sector size      : ${drive_sector_sizes[$drive]}
-#   Size (Bytes)     : $(( (${end_sector} - ${start_sector}) * ${drive_sector_sizes[$drive]} ))
-#   Size (GiB)       : $(( (${end_sector} - ${start_sector}) * ${drive_sector_sizes[$drive]} / 1024 / 1024 / 1024 ))
-# EOF
-
-
-
   local biggest_drive
   local second_biggest_drive
+  local biggest_size=0
+  local second_biggest_size=0
+  for drive in "${local_drives[@]}"; do
+    __avail=$(( (${drive_end_sectors[$drive]} - ${drive_start_sectors[$drive]}) ))
+    if (( ${__avail} > biggest_size )); then
+      second_biggest_size=${biggest_size}
+      second_biggest_drive=${biggest_drive}
+      biggest_size=${__avail}
+      biggest_drive=${drive}
+    elif (( ${__avail} > second_biggest_size )); then
+      second_biggest_size=${__avail}
+      second_biggest_drive=${drive}
+    fi
+  done
 
+  logInfo << EOF
+The biggest drive is ${biggest_drive} with ${biggest_size} sectors
+The second biggest drive is ${second_biggest_drive} with ${second_biggest_size} sectors
+EOF
+
+  if [[ ${second_biggest_size} -lt $(( 10 * 1024 * 1024 * 1024 / 512 )) ]]; then
+    logError "We need at least two drives with 10+ GiB of space available"
+    return 1
+  fi
+
+  # Curreny only support the second device being leftover space from a raid array
+  if [[ ${drive_start_sectors[$biggest_drive]} -le 0 ]]; then
+    logError "Don't know how to configure with an empty drive"
+    return 1
+  fi
+
+  # Currently only support the first device being an empty drive
+  if [[ ${drive_start_sectors[$second_biggest_drive]} -ne 0 ]]; then
+    logError "Can only configure using an empty drive"
+    return 1
+  fi
+
+  # Save the configuration for the VM_STORAGE drive
+  if ! config_save "${STOR_FILE}" VM_STOR_DRIVE1 "${second_biggest_drive}"; then
+    logError "Failed to save VM_STOR_DRIVE1"
+    return 1
+  elif ! config_save "${STOR_FILE}" VM_STOR_DRIVE2 "${biggest_drive}"; then
+    logError "Failed to save VM_STOR_DRIVE2"
+    return 1
+  elif ! config_save "${STOR_FILE}" VM_STOR_DRIVE1_START "${drive_start_sectors[$second_biggest_drive]}"; then
+    logError "Failed to save VM_STOR_DRIVE1_START"
+    return 1
+  elif ! config_save "${STOR_FILE}" VM_STOR_DRIVE2_START "${drive_start_sectors[$biggest_drive]}"; then
+    logError "Failed to save VM_STOR_DRIVE2_START"
+    return 1
+  elif ! config_save "${STOR_FILE}" VM_STOR_SIZE "${second_biggest_size}"; then
+    logError "Failed to save VM_STOR_SIZE"
+    return 1
+  elif ! config_save "${STOR_FILE}" VM_STOR_DRIVE "md10"; then
+    logError "Failed to save VM_STOR_DRIVE"
+    return 1
+  else
+    logInfo "Storage configuration saved for VM storage"
+  fi
+
+  # Adjust Biggest drive
+  drive_start_sectors[$biggest_drive]=$(( ${drive_start_sectors[$biggest_drive]} + ${second_biggest_size} ))
+
+  # Calculate parameters for ISO_STOR, alignned to the next physical sector
+  local iso_store_start=${drive_start_sectors[$biggest_drive]}
+  local align_value=$(( ${drive_physical_sectors[$biggest_drive]} / 512 ))
+  if (( ${align_value} * 512 !=  ${drive_physical_sectors[$biggest_drive]} )); then
+    logWarn "Physical sector size is not a multiple of 512"
+  else
+    iso_store_start=$(( ${iso_store_start} + ${align_value} - (${iso_store_start} % ${align_value}) ))
+  fi
+  local iso_store_size=$(( ${drive_end_sectors[$biggest_drive]} - ${iso_store_start} ))
+
+  # Make sure ISO storage is at least 5 GiB in size
+  if [[ ${iso_store_size} -lt $(( 5 * 1024 * 1024 * 1024 / 512 )) ]]; then
+    logError "Not enough space for ISO storage"
+    return 1
+  fi
+
+  logInfo << EOF
+ISO storage configuration:
+  Device: ${biggest_drive}
+  Start sector: ${iso_store_start}
+  Size: ${iso_store_size} sectors ($(( ${iso_store_size} * 512 / 1024 / 1024 / 1024 )) GiB)
+EOF
+
+  # Save the configuration for the ISO storage drive
+  if ! config_save "${STOR_FILE}" ISO_STOR_DRIVE "${biggest_drive}"; then
+    logError "Failed to save ISO_STOR_DRIVE"
+    return 1
+  elif ! config_save "${STOR_FILE}" ISO_STOR_START "${iso_store_start}"; then
+    logError "Failed to save ISO_STOR_START"
+    return 1
+  elif ! config_save "${STOR_FILE}" ISO_STOR_SIZE "${iso_store_size}"; then
+    logError "Failed to save ISO_STOR_SIZE"
+    return 1
+  elif ! config_save "${STOR_FILE}" ISO_STOR_PATH "/mnt/iso_store"; then
+    logError "Failed to save ISO_STOR_PATH"
+    return 1
+  else
+    logInfo "Storage configuration saved for ISO storage"
+  fi
+
+  # Commit this design
+  if ! config_save "${STOR_FILE}" STOR_STATE designed; then
+    logError "Failed to save STOR_STATE"
+    return 1
+  else
+    logInfo "Storage design saved"
+  fi
 
   return 0
 }
